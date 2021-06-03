e2437ab17e09374fe747a86298869d051f05c0ad
Clark Kent <razvedchik008@gmail.com>
2021-06-04 00:00:40 +0300

    Updates

---
+++ precache-manifest.126edeb7b31967640f4cbf902c050acd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a04d19628afa7e2d17a8ee84de7360d",
+    "url": "/ToDlist/index.html"
+  },
+  {
+    "revision": "d7cc8eefc2a41f8976ae",
+    "url": "/ToDlist/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "351589764cdc150dc8ae",
+    "url": "/ToDlist/static/js/2.2a516cfd.chunk.js"
+  },
+  {
+    "revision": "465376c0c4b9728ff624339d576d7e27",
+    "url": "/ToDlist/static/js/2.2a516cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7cc8eefc2a41f8976ae",
+    "url": "/ToDlist/static/js/main.89b2f4c1.chunk.js"
+  },
+  {
+    "revision": "5807bef1a919f6ce5277",
+    "url": "/ToDlist/static/js/runtime-main.dba80a35.js"
+  }
+]);
